4b6b216e095c9a257803d7fefee280afb3d5d4c7
heislercreative <brennan@heislercreative.com>
2019-02-02 21:02:41 -0700

    update video size

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ import placeholder from '../images/placeholder.jpg'
 const Video = ({ name, key, site, embed_path, link_path }) => {
   return(
     <Segment floated='left' className='video-card'>
-      <iframe width="267" height="150" src={embed_path} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="266" height="150" src={embed_path} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       <div className='video-title'>
         <a href={link_path}>
           <strong>{name}</strong>
