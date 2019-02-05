72d36b6e4b558c54cb5e046776f40b6f96959ba5
heislercreative <brennan@heislercreative.com>
2019-02-04 19:07:42 -0700

    clean up Video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Segment, Image } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react'
 
-import placeholder from '../images/placeholder.jpg'
-
-const Video = ({ name, key, site, embed_path, link_path }) => {
+const Video = ({ name, embed_path, link_path }) => {
   return(
     <Segment floated='left' className='video-card'>
-      <iframe width="266" height="150" src={embed_path} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe title={name} width="266" height="150" src={embed_path} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       <div className='video-title'>
         <a href={link_path}>
           <strong>{name}</strong>
