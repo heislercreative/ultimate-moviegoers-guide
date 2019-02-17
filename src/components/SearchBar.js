844f2dceffb9b51ce901b066d0075a7142634fa7
heislercreative <brennan@heislercreative.com>
2019-02-16 20:12:18 -0700

    refactor to use history.push instead of Redirect

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { Input, Icon } from 'semantic-ui-react'
 
 class SearchBar extends Component {
@@ -8,8 +8,7 @@ class SearchBar extends Component {
     super()
     this.state = {
       text: '',
-      query: '',
-      redirect: false
+      query: ''
     }
   }
 
@@ -17,49 +16,42 @@ class SearchBar extends Component {
     this.setState({ text: e.target.value })
   }
 
-  setQuery = () => {
-    this.setState({
+  renderSearch = async () => {
+    await this.setState({
       query: this.state.text.replace(/ /g,'+'),
-      redirect: true
     })
-  }
-
-  renderRedirect = () => {
-    this.setState({
-      text: '',
-      redirect: false
-    })
-    return <Redirect push to={`/search/${this.state.query}`}/>
+    if (this.state.query !== '') {
+      this.props.history.push(`/search/${this.state.query}`)
+      this.setState({
+        text: ''
+      })
+    }
   }
 
   handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.setQuery()
+      this.renderSearch()
     }
   }
 
   handleClick = () => {
-    this.setQuery()
+    this.renderSearch()
   }
 
   render() {
-
     return(
-      <div>
-        {this.state.redirect && this.renderRedirect()}
-        <Input
-          name='text'
-          type='text'
-          placeholder='Search movies...'
-          icon={<Icon name='search' link onClick={this.handleClick}/>}
-          value={this.state.text}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-          className='search-bar'
-        />
-      </div>
+      <Input
+        name='text'
+        type='text'
+        placeholder='Search movies...'
+        icon={<Icon name='search' link onClick={this.handleClick}/>}
+        value={this.state.text}
+        onChange={this.handleChange}
+        onKeyPress={this.handleKeyPress}
+        className='search-bar'
+      />
     )
   }
 }
 
-export default SearchBar
+export default withRouter(SearchBar)
