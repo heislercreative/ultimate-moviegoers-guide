6569b75b30bb8615dcde87e68adf93ecf1c0fd6a
heislercreative <brennan@heislercreative.com>
2019-02-16 17:37:43 -0700

    open video links in new tab

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ const Video = ({ name, embed_path, link_path }) => {
     <Segment floated='left' className='video-card'>
       <iframe title={name} width="266" height="150" src={embed_path} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       <div className='video-title'>
-        <a href={link_path} target='_blank'>
+        <a href={link_path} target='_blank' rel="noopener noreferrer">
           <strong>{name}</strong>
         </a>
       </div>
